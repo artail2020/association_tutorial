7ad773cd1fd6b65fc79e57418a74975e5dcaf25e
artail2020 <artail2012@gmail.com>
2020-12-13 00:57:22 +0900

    tutorial end

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+  root 'tweets#index'
+  devise_for :users
+
+  resources :tweets do
+    resource :favorites, only: [:create, :destroy]
+    resource :comments, only: [:create, :destroy]
+  end
+
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
